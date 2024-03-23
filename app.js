f879b60d1cafd4edfb5a982e8adadcaf535effe5
olenteeth <idvietnam2324@gmail.com>
2024-03-23 09:08:21 +0700

    server

--- app.js
+++ app.js
@@ -26,28 +26,3 @@ wss.on('connection', function connection(nhantin) {
 server.listen(3000, function listening() {
   console.log('Server started on port 3000');
 });
-/*express = require('express');
-const WebSocket = require('ws');
-
-const PORT = 3000;
-
-const wsServer = new WebSocket.Server({
-    port: PORT
-});
-
-const app = express();
-
-app.use('/',express.static('public'));
-wsServer.on('connection', function(nhantin){
-    console.log("Ayo co nguoi moi vao");
-
-    nhantin.on('message', function(test){
-        console.log("Nhận tin từ Client nào đó:" + test);
-        
-        wsServer.clients.forEach(function(client){
-            client.send("Ai đó vừa nói:" + test);
-        });
-    });
-});
-
-console.log((new Date()) + "Server đang hoạt động ở port: " + PORT);*/
