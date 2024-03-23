c189ae4779bf3e229bff0ff14148c778e7edb99d
olenteeth <idvietnam2324@gmail.com>
2024-03-23 08:58:58 +0700

    server

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*express = require('express');
+express = require('express');
 const WebSocket = require('ws');
 
 const app = express();
@@ -7,23 +7,26 @@ const wss = new WebSocket.Server({ server:server });
 
 app.use('/',express.static('public'));
 
-wss.on('connection', function connection(ws) {
-  console.log('Client connected');
+wss.on('connection', function connection(nhantin) {
+  console.log("Ayo co nguoi moi vao");
 
-  ws.on('message', function incoming(message) {
-    console.log('Received: %s', message);
-    // Forward the message to ESP32
-  });
+  nhantin.on('message', function(test){
+    console.log("Nhận tin từ Client nào đó:" + test);
+    
+    wss.clients.forEach(function(client){
+        client.send("Ai đó vừa nói:" + test);
+    });
+});
+});
 
   ws.on('close', function close() {
     console.log('Client disconnected');
   });
-});
 
 server.listen(3000, function listening() {
   console.log('Server started on port 3000');
-});*/
-express = require('express');
+});
+/*express = require('express');
 const WebSocket = require('ws');
 
 const PORT = 3000;
@@ -47,4 +50,4 @@ wsServer.on('connection', function(nhantin){
     });
 });
 
-console.log((new Date()) + "Server đang hoạt động ở port: " + PORT);
+console.log((new Date()) + "Server đang hoạt động ở port: " + PORT);*/
