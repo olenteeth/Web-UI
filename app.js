cc805fcf7f118a8f5772eed37a442239efa2cd27
olenteeth <idvietnam2324@gmail.com>
2024-04-13 00:19:08 +0700

    super change

--- app.js
+++ app.js
@@ -10,14 +10,13 @@ app.use('/',express.static('public'));
 wss.on('connection', function connection(nhantin) {
   console.log("Ayo co nguoi moi vao");
 
-  nhantin.on('message', function(test){
+  nhantin.on('message', function incoming(test){
     console.log("Nhận tin từ Client nào đó:" + test);
-    
-    wss.clients.forEach(function(client){
-        client.send("Ai đó vừa nói:" + test);
+      wss.clients.forEach(function(client){
+        client.send(String(test));
+      });
     });
-});
-});
+    });
 
   wss.on('close', function close() {
     console.log('Client disconnected');
